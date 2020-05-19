07278872a928063e65e26dec7fb959efa7a9d8a7
Markus Andresen <markus.andresen94@gmail.com>
2020-05-19 16:20:28 +0200

    --wip-- [skip ci]

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -37,12 +37,60 @@ resource "aws_s3_bucket_policy" "b2" {
 }
 
 
-# resource "aws_cloudfront_distribution" "s3_distribution" {
-#   origin {
-#     domain_name = "${aws_s3_bucket.b1.bucket_regional_domain_name}"
-#     origin_id   = "${local.s3_origin_id}"
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "${aws_s3_bucket.b_assets.bucket_regional_domain_name}"
+    origin_id   = "${aws_s3_bucket.b_assets.id}"
+  }
+  
+  origin {
+    domain_name = "${aws_s3_bucket.b_config.bucket_regional_domain_name}"
+    origin_id   = "${aws_s3_bucket.b_config.id}"
+  }
 
-#     s3_origin_config {
-#       origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
-#     }
-#   }
+  enabled = true
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "${aws_s3_bucket.b_config.id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+
+  }
+  
+  ordered_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    path_pattern     = "/assets/*"
+    target_origin_id = "${aws_s3_bucket.b_assets.id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "redirect-to-https"
+  }
+}
